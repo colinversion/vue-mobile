48dd8965863d4c5b8fc4c3cb8c4556e9a22ef3c9
colin <colin@ay033.com>
2018-11-29 11:04:17 +0800

    增加公共样式，页面加载等待效果

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import store from '@/store'
 
+import '@/assets/css/common.css'
 import 'lib-flexible'
 import Cube from 'cube-ui'
 import api from '@/api'
@@ -15,10 +16,16 @@ Vue.prototype.$api = api
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+let _instance = new Vue({
   el: '#app',
   router,
   store,
   components: { App },
   template: '<App/>'
+})
+
+Vue.prototype.$loading = _instance.$createToast({
+  txt: 'Loading...',
+  mask: true,
+  time: 0
 })
